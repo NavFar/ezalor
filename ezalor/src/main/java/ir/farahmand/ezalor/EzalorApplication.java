feef257d42f35d240056ea6ac4badf3c0d93b3e2
navidfarahmand <navidfarahmand75@gmail.com>
2022-11-12 05:03:22 +0330

    add main project

---
+++ ezalor/src/main/java/ir/farahmand/ezalor/EzalorApplication.java
@@ -0,0 +1,13 @@
+package ir.farahmand.ezalor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzalorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzalorApplication.class, args);
+	}
+
+}
---
+++ ezalor/src/test/java/ir/farahmand/ezalor/EzalorApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.farahmand.ezalor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzalorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
