f748f89c87d1d4c289789d1ad2482e64ec858af1
navidfarahmand <navidfarahmand75@gmail.com>
2022-11-14 10:24:43 +0330

    add type to target model

--- ezalor/src/main/java/ir/farahmand/ezalor/controllers/Type1RegisterController.java
+++ ezalor/src/main/java/ir/farahmand/ezalor/controllers/Type1RegisterController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ir.farahmand.ezalor.DAOs.T1RegisterDAO;
 import ir.farahmand.ezalor.models.Target;
+import ir.farahmand.ezalor.models.Target.Type;
 import ir.farahmand.ezalor.services.TargetStoreService;
 
 @RestController
@@ -38,6 +39,7 @@ public class Type1RegisterController {
 		for(String targetURL: input.getTargets()) {
 			Target target = new Target();
 			target.setUrl(targetURL);
+			target.setType(Type.TYPE1);
 			this.targetStoreService.addTarget(uuid, target);
 		}
 		return new ResponseEntity<>(uuid,HttpStatus.OK);
--- ezalor/src/main/java/ir/farahmand/ezalor/models/Target.java
+++ ezalor/src/main/java/ir/farahmand/ezalor/models/Target.java
@@ -2,15 +2,28 @@ package ir.farahmand.ezalor.models;
 
 import java.io.Serializable;
 
+
+
 public class Target implements Serializable {
+	public enum Type {
+		TYPE1,
+		TYPE2,
+		TYPE3
+	}
 
 	private static final long serialVersionUID = 1930734022682781487L;
 	private String url;
-	
+	private Type type;
 	public String getUrl() {
 		return url;
 	}
 	public void setUrl(String url) {
 		this.url = url;
+	}
+	public Type getType() {
+		return type;
+	}
+	public void setType(Type type) {
+		this.type = type;
 	}
 }
