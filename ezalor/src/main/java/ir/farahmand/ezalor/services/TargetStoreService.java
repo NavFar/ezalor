931f4f6ebf03a487863ee427244fd97e17b161d7
navidfarahmand <navidfarahmand75@gmail.com>
2022-12-28 16:08:06 +0330

    add execute target to get result

--- ezalor/src/main/java/ir/farahmand/ezalor/services/TargetStoreService.java
+++ ezalor/src/main/java/ir/farahmand/ezalor/services/TargetStoreService.java
@@ -7,20 +7,23 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import ir.farahmand.ezalor.models.Target;
 import ir.farahmand.ezalor.models.Target.Type;
+import reactor.core.publisher.Mono;
 
 @Service
 public class TargetStoreService {
 
 	private Map<String, List<Target>> targets;
-	
+	private WebClient webClient;
 	public TargetStoreService() {
 		this.targets = new HashMap<>();
+		this.webClient = WebClient.create();
 	}
 	
-	public String createTarget(Type type, String[] targets) {
+	public String createTargets(Type type, String[] targets) {
 		String uuid = UUID.randomUUID().toString();
 		for(String targetURL: targets) {
 			Target target = new Target();
@@ -37,5 +40,13 @@ public class TargetStoreService {
 			selectedTarget = new ArrayList<Target>();
 		selectedTarget.add(target);
 		this.targets.put(key, selectedTarget);
+	}
+	
+	public List<Target> getTargets(String uuid){
+		return this.targets.get(uuid);
+	}
+	
+	public Mono<String> executeTarget(Target target){
+		return this.webClient.get().uri(target.getUrl()).retrieve().bodyToMono(String.class).map(response-> target.getUrl() + ": {" + response + "}");
 	}
 }
